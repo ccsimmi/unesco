6b953e2a2bef0d5dd898520a6c50356293c62bcd
ccsimmi <ffsimmi@gmail.com>
2024-07-02 17:26:18 +0100

    initial project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("index", {
+		title: "Hello World!",
+	});
+});
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
---
+++ src/database/db.js
@@ -0,0 +1,12 @@
+const pg = require("pg");
+const { Pool } = pg;
+
+const pool = new Pool({
+	host: "database",
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+	port: process.env.POSTGRES_PORT,
+});
+
+module.exports = pool;
